e4457eaa104140db878d7b5adcb8d44ebc72aabc
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-04-14 15:41:24 -0600

    added some seeds for the tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,22 @@
+DROP DATABASE IF EXISTS company;
+/* if the database already exists, then delete it.*/
+
+CREATE DATABASE company;
+/* create the company database */
+
+USE company;
+/* use the company database */
+
 CREATE TABLE department (
      id INTEGER AUTO_INCREMENT PRIMARY KEY,
      name VARCHAR(30) NOT NULL
     );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(20,2) NOT NULL,
-    department_id INTEGER NOT NULL 
+    department_id INTEGER NOT NULL
     );
 
 CREATE TABLE employee (
---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (name)
+VALUES
+('Sales'), 
+('Engineering'), 
+('Finance'), 
+('Legal'), 
+('Management');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Salesperson', 500, 1), 
+('Sales Lead', 1000, 1), 
+('Lead Engineer', 2000, 2), 
+('Software Engineer', 1500, 2), 
+('Accountant', 1200, 3),
+('Legal Team Lead', 1500, 4),
+('Lawyer', 2200, 4); 
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Jack', 'Frost', 4, 0),
+('Commander', 'Shepard', 7, 0),
+('Jane', 'Doe', 3, 0),
+('Oliver', 'Tree', 5, 0),
+('Matthew', 'Bellamy', 6, 0),
+('Hussein', 'Farran', 4, 0),
+('Ash', 'Ketchum', 1, 0),
+('Deckard', 'Cain', 2, 0),
+('Guy', 'Fieri', 1, 0)
